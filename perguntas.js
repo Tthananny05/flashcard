cd7bebcebdcfb6290dee4cf4f158e7f291dd5c69
ThawanySouza <00001125512313sp@al.educacao.sp.gov.br>
2024-11-29 01:37:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Canção',
+    'Batatinha quando nasce, esparrama pelo chão, menininha quando dorme...',
+    'põe a mão no coração'
+)
+
+criaCartao(
+    'sobre Thawany',
+    'Qual a cor dos olhos de Thawany?',
+    'Castanhos claro'
+)
+
+criaCartao(
+    'Gosta de fazer',
+    'O que Thawany gosta de fazer?',
+    'Comer,sair para lugares diferentes e curtir'
+)
+
+criaCartao(
+    'Volei',
+    'O que é considerado passe A no voleibol?',
+    'Passe na mão do levantador'
+)
